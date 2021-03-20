32906d370d50e6c40ee69db9d9f5bc3a323a3e36
koshi-jp <koushi0324@gmail.com>
2021-03-20 12:26:17 +0000

    320

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,15 @@
+class BooksController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+
+end
---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,15 @@
+class HomesController < ApplicationController
+
+  def top
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+end
