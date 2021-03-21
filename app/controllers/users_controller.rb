39257c52276da32563a225733c7e12f14d3d8092
koshi-jp <koushi0324@gmail.com>
2021-03-21 15:49:18 +0000

    322

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,16 @@ class UsersController < ApplicationController
   end
 
   def edit
+     @user = User.find(params[:id])
+  end
 
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
   end
 
   def book_params
-    params.require(:book).permit(:title, :body, :user_id)
+    params.require(:book).permit(:title, :body,)
   end
 end
