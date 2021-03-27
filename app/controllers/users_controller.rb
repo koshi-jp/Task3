a83eab99b83b64499f0bd9f246ea480295976f29
koshi-jp <koushi0324@gmail.com>
2021-03-27 03:56:22 +0000

    328

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,8 +22,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    # @user.update(user_params)
-    # redirect_to user_path(@user.id)
+    
     if @user.update(user_params)
       flash[:notice] = 'You have update user info successfully.'
       redirect_to user_path(@user)
